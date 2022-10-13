32449c90f054cbb65840578f13a782be4a63400f
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-13 16:29:16 -0700

    final

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - prints a message to standard error
  * Return: returns one
  */
 int main(void)
 {
-	fprintf(stderr,  "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
-);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+60);
 	return (1);
 }
