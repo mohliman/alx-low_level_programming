41b0d9a905dc08a77d3f83d6dbeef70da5eaa360
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-13 14:53:58 -0700

    new C file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints a message to standard output
+ * Return: returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
