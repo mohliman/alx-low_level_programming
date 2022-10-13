6ae8f99dd6ad8b35973c0496b7626bd37e373be5
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-13 14:45:08 -0700

    new C file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This function outputs a text
+ * Return: returns zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
