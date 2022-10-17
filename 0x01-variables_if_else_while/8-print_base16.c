3792a6a95a19021cb5ec0e7ea00e54fedb153d8d
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-17 16:22:34 -0700

    new script

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: zero
+ */
+int main(void)
+{
+	int num;
+	char alpha;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
