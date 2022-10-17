3318af6d332cb154bdc95c23e90e680966ba11f0
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-17 16:10:57 -0700

    new script

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: zero
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
