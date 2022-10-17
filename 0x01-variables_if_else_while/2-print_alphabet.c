696f5da3a8903ea443bb32e13dd6037532ddc8c2
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-17 11:33:51 -0700

    new script

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -returns the alphabet in lowercase
+ * Return: zero
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i < 'z'; i++)
+	{
+	printf("%c", i);
+	};\n
+}
