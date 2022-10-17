cd86132cce5ee143fd8ca2d6c2d5287087f3cab8
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-17 16:01:27 -0700

    new script

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - outputs numbers 0-9 with putchar function
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar((i % 10) + '0');
+	}
+	putchar('\n');
+	return(0);
