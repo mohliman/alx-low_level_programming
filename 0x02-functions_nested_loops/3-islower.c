3a1dacdabaeb8b41d16cd8cb9b1b2936ec7f19e7
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-19 13:50:15 -0700

    added new script

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - entry point
+ * @c: parameter of _islower function
+ * Return: one or zero
+ */
+int _islower(int c)
+{
+	if (c == islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
