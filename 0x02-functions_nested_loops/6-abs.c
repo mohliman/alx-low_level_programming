8b3719c40d749da2ab619484ab3e3b6ea99fb53f
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-19 16:26:50 -0700

    added new script

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - entry point
+ * Return: absolute value
+ * @n: argument
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return(-n);
+	}
+}
