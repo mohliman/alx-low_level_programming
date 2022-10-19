6bc200f0f952d049c6d8179074e496adae764fa9
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-19 13:16:25 -0700

    added new script

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ * Return: zero
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
