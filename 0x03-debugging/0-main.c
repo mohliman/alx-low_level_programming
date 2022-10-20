3095ec8b1d0c73f7b37d11214cf21923d37d7bc8
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-20 11:03:00 -0700

    added new script

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * positive_or_negative - entry point
+ * @i: argument
+ */
+void positive_or_negative(int i)
+{
+	positive_or_negative(0);
+	printf("\n");
+}
