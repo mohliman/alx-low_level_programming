92a7569b5913e5dfcd22eefc99744f54befe23f8
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-19 17:25:33 -0700

    added new script

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add  - adds two numbers and outputs result
+ * @a: parameter
+ * @b: parameter
+ * Return: total
+ */
+int add(int a, int b)
+{
+	int total;
+       
+	total =	a + b;
+
+	return (total);
+}
+
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main"
+/**
+ * jack_bauer - entry point
+ */
+void jack_bauer(void)
+{
+	int a = 0, b;
+	
+	while (a < 24)
+	{
+		b = 0;
+		while ( b < 60)
+		{
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
+			b++;
+		}
+		a++
+	}
+}
