b1a9dfaf5573897ad3afc6ad80ea9a4b9ddc448d
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-20 15:01:52 -0700

    added new script

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - entry point
+ */
+void print_most_numbers(void)
+{
+	char a;
+
+	for (a = 48; a < 58; a++)
+	{
+		if (a == 50)
+		{
+			continue;
+		}
+		if (a == 52)
+		{
+			continue;
+		}
+		_putchar(a);
+	}
+}
