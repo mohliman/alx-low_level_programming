3bd2a686bd10dcdc18f43c44a76a01a81a5b9efc
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-20 12:39:25 -0700

    added new script

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - entry point
+ * @a: parameter one
+ * @b: parameter two
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
