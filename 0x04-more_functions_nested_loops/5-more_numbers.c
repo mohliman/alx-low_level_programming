e49522e2d02164706944daf2c232284fa731d891
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-20 15:42:21 -0700

    added new script

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,10 +8,20 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 48; j < 58; j++)
 		{
 			_putchar(j);
 		}
+		_putchar(49);
+		_putchar(48);
+		_putchar(49);
+		_putchar(49);
+		_putchar(49);
+		_putchar(50);
+		_putchar(49);
+		_putchar(51);
+		_putchar(49);
+		_putchar(52);
 		_putchar('\n');
 	}
 }
