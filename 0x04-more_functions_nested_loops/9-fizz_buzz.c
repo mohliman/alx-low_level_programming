b85b57af48d0e89b54641955caabbb93f2c070a6
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-21 16:49:18 -0700

    added new script

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,20 +10,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 5 == 0 && i % 3 == 0)
-		{
-			printf(" FizzBuzz");
-			continue;
-		}
-		else if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf(" Fizz");
-			continue;
 		}
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf(" Buzz");
-			continue;
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
 		}
 		else
 		{
