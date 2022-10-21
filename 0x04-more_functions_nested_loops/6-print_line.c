dacbfd7e0282fe3b07cea260a062367f22a165e5
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-21 11:45:44 -0700

    added new script

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - entry point
+ * @n: parameter of function
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
