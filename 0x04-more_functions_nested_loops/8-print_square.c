e65d1903f9bc67dd9493c1982c5b2b64e3bd485b
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-21 12:09:21 -0700

    added new script

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - entry point
+ * @size: parameter of function
+ */
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+}
