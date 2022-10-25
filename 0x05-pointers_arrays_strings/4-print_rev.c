481ee599272cb7ddbc176c052ad6ca8b66f1abc5
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-24 17:38:43 -0700

    added new script

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,15 @@
 void print_rev(char *s)
 {
 	int i, j, k;
+	char ch[];
 
 	i = 0;
+	j = 0;
 	while (s[i] != '\0')
-		i++;
-	j = i;
-	for (k = j - 1; k >= 0; k--)
-		_putchar(s[j]);
+		ch[] = s[i];
+		j += 1;
+	for (k = ch[j]; k >= 0; k--)
+		_putchar(ch[j]);
 	_putchar('\n');
 
 }
