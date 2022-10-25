d14a73e42d903a9e29239db2176d21e388e2f716
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-25 16:52:14 -0700

    added new script

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - entry point
+ * @str: parameter of function
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
