f39d487b9315a8fdd0470ccb0870ab5fe5e2ba4f
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-25 17:34:42 -0700

    added new script

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Return: zero;
+ */
+int main(void)
+{
+	int myrand, count, total;
+
+	srand(tie(NULL));
+	for (count = 0; total = 2772; total > 122; count++)
+	{
+		myrand = (rand() % 127);
+		printf("%c", myrand);
+		total -= myrand;
+	}
+	printf("%c", total);
+	return (0);
+}
