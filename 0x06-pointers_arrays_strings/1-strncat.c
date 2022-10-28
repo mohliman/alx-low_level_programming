b7089ff0b1b40b10b796aee4cd37321843b8678c
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-27 17:01:12 -0700

    added new script

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strncat - entry point
+ * @dest: param one
+ * @src: param two
+ * @n: param three
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
