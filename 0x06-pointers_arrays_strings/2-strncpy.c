567af2cc6f59eaaaacc1050207d922387f8fb3cb
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-27 17:20:18 -0700

    added new script

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strncpy - entry point
+ * @dest: param one
+ * @src: param two
+ * @n: param three
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+	return (dest);
+}
