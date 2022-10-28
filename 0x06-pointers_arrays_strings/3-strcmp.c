be4d89a9f2a451e8f8ad81f499ad27d18955e953
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-10-27 17:28:34 -0700

    added new script

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - entry point
+ * @s1: param one
+ * @s2: param two
+ * Return: difference between s1 and s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	i++;
+	}
+	return (0);
+}
