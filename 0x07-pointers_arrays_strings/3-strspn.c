538bc5a834e2dcb7e1500c55cdac7f41d5328141
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-11-01 15:42:39 -0700

    added new script

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strspn - entry point
+ * @s: source string
+ * @accept: accepted string
+ * Return: number of bytes in the init segment
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a = 0; b, t = 0;
+
+	while (accept[a])
+	{
+		b = 0;
+
+		while (s[b] != 32)
+		{
+			if (accept[a] == s[b])
+			{
+				t++;
+			}
+			b++;
+		}
+		a++;
+	}
+	return (t);
+}
