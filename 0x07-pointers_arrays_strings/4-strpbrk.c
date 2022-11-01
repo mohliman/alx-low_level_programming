f824d532552be9269e42d85e1d85c1e4e8b00ad8
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-11-01 15:51:56 -0700

    added new script

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *_strpbrk - entry point
+ * @s: src string
+ * @accept: accepted characters
+ * Return: string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a = 0, b;
+
+	while (s[a])
+	{
+		b = 0;
+
+		while (accept[b])
+		{
+			if (s[a] == accept[b])
+			{
+				s += a;
+				return (s);
+			}
+			b++;
+		}
+		a++;
+	}
+	return ('\0');
+}
