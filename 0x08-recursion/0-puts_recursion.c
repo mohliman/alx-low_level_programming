6a45ad4ee01f1bf79a805cf748e35509c0068d3f
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2022-11-02 01:01:21 -0700

    added new script

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s - string to print
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		_putchar(a[i]);
+		i++;
+	}
+	_putchar('\n');
+}
